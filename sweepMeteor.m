6a2341822b38e5b20bb14c5667f4199a5b29b256
ljordan51 <ljordan51@gmail.com>
2016-12-03 17:07:23 -0500

    edited graph results

--- sweepMeteor.m
+++ sweepMeteor.m
@@ -1,12 +1,12 @@
 close all
 
-n = 101;
-earthV = 30.3e3;
+n = 601;
+earthV = 3.03e4;
 earthM = 5.972e24;
-meteorVsmall = 5e8;
-meteorVbig = 100.3e7;
-meteorMsmall = 4e19;
-meteorMbig = 1e20;
+meteorVsmall = 4.3e4;
+meteorVbig = 5.8e5;
+meteorMsmall = 1e21;
+meteorMbig = 14e24;
 meteorVs = linspace(meteorVsmall,meteorVbig,n);
 meteorMs = linspace(meteorMsmall,meteorMbig,n);
 meteorVstep = (meteorVbig - meteorVsmall)/(n-1);
@@ -40,12 +40,26 @@ for i = 1:n
     end
 end
 
-b = [0 0 0; 0 1 0];
+b = [0 0 0; 66/255 170/255 244/255];
 colormap(b);
 pcolor(meteorMs, meteorVs, E);
 shading flat
 
-ylabel('Speed of Meteor (m/s)')
-xlabel('Mass of Meteor (kg)')
+set(gca,'FontSize',15);
+ylabel('Velocity of Comet (m/s)')
+xlabel('Mass of Comet (kg)')
 
-title('Earth Speed After Inelastic Collision vs. Meteor Speed and Mass')
+title('Earth Velocity After Inelastic Collision Given Comet Velocity and Mass')
+% text(meteorMs(26),meteorVs(n-50),'Upper velocity is equal to that of the fastest comet recorded', 'Color', 'white', 'FontSize', 15)
+% text(meteorMs(26),meteorVs(n-100),'Lower velocity is equal to the escape velocity of the Earth from the Sun', 'Color', 'white', 'FontSize', 15)
+hold on
+plot([earthM/10 earthM/10],[0 meteorVs(50)],'w-','LineWidth', 2);
+text(earthM/10,meteorVs(60),'0.1 x Earth Mass', 'Color', 'white', 'FontSize', 15)
+plot([earthM/2 earthM/2],[0 meteorVs(50)],'w-','LineWidth', 2);
+text(earthM/2,meteorVs(60),'0.5 x Earth Mass', 'Color', 'white', 'FontSize', 15)
+plot([earthM earthM],[0 meteorVs(50)],'w-','LineWidth', 2);
+text(earthM,meteorVs(60),'1 x Earth Mass', 'Color', 'white', 'FontSize', 15)
+plot([earthM*2 earthM*2],[0 meteorVs(50)],'w-','LineWidth', 2);
+text(earthM*2,meteorVs(60),'2 x Earth Mass', 'Color', 'white', 'FontSize', 15)
+
+legend('Earth Breaks Orbit');
